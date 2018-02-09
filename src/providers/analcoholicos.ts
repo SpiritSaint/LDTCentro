136f3cb40ba259a9d2994f86fec7052dae8c3959
Ian Torres <iantorres@outlook.com>
2018-02-09 15:50:10 -0300

    Precio reestablecido

--- src/providers/analcoholicos.ts
+++ src/providers/analcoholicos.ts
@@ -17,7 +17,7 @@ export class Analcoholicos {
         title: 'Agua Mineral Italiana',
         photo: 'assets/img/default.jpg',
         description: 'Acqua Panna - San Pellegrino', 
-        price: 500
+        price: 2600
       },
       { 
         title: 'Jugos',
